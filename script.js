9a6559701ad7a59eb6a2424c671f94d303812cfc
Fredrik Uddenfeldt <fredrikuddenfeldt@fredriks-air.lan>
2023-07-03 22:30:12 +0200

    new buttons

---
+++ script.js
@@ -0,0 +1,6 @@
+const AC = document.getElementById('AC')
+const seven = document.getElementById('seven')
+
+AC.addEventListener ('click', () => console.log("AC"))
+
+seven.addEventListener ('click', () => console.log("7"))
