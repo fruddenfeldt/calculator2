5d0da5e77a6fe40409ff190030973e3cb200934c
Fredrik Uddenfeldt <fredrikuddenfeldt@fredriks-air.lan>
2023-07-11 22:28:52 +0200

    improved buttons

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const AC = document.getElementById('AC')
+
+const one = document.getElementById('one')
+const two = document.getElementById('two')
+const three = document.getElementById('three')
+const four = document.getElementById('four')
+const five = document.getElementById('five')
+const six = document.getElementById('six')
 const seven = document.getElementById('seven')
+const eight = document.getElementById('eight')
+const nine = document.getElementById('nine')
+const zero = document.getElementById('zeroButton')
+const inputArray = []
 
-AC.addEventListener ('click', () => console.log("AC"))
+AC.addEventListener ('click', () => 
+    location.reload()
+    )
 
-seven.addEventListener ('click', () => console.log("7"))
+zero.addEventListener ('click', () => {
+    inputArray.push(7);
+    console.log("zero")
+    
+        })
