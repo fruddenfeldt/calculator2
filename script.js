3a26190c4fcf848c0efab8001809b9318aa2eb54
Fredrik Uddenfeldt <fredrikuddenfeldt@fredriks-air.lan>
2023-07-14 22:24:14 +0200

    v.01 of addition function

--- script.js
+++ script.js
@@ -10,14 +10,40 @@ const seven = document.getElementById('seven')
 const eight = document.getElementById('eight')
 const nine = document.getElementById('nine')
 const zero = document.getElementById('zeroButton')
+
+const additionButton = document.getElementById('additionButton')
 const inputArray = []
+let firstNumber;
+let secondNumber;
+
+function operator (firstNumber, secondNumber) {
+
+}
 
 AC.addEventListener ('click', () => 
     location.reload()
     )
 
-zero.addEventListener ('click', () => {
+seven.addEventListener ('click', () => {
     inputArray.push(7);
-    console.log("zero")
+    firstNumber = 7;
+    console.log(firstNumber)
+        })
+
+eight.addEventListener ('click', () => {
+    inputArray.push(8);
+    secondNumber = 8;
+    console.log(secondNumber)
     
         })
+
+
+additionButton.addEventListener('click', () => {
+    console.log('plus!');
+    console.log(add(firstNumber, secondNumber))
+})
+
+const add = function(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+        };
+        
